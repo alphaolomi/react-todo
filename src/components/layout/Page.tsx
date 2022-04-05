7add5ddc1b114c3f79ccf8063c55f5d1586cd014
Alpha Olomi <alphaolomi@gmail.com>
2022-04-05 23:55:48 +0300

    add margins styles

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -2,16 +2,25 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 interface PageProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
-const Page = ({children} :PageProps) => {
-    return (
-        <div>
-            <Header />
-            {children}
-            <Footer/>
-        </div>
-    )
-}
+
+const Page = ({ children }: PageProps) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+        width: "50%",
+        margin: "auto",
+      }}
+    >
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
 
 export default Page;
