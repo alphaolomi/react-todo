249eca3e32709f2ad622b9dc0e111b48f022bbb0
Alpha Olomi <alphaolomi@gmail.com>
2022-04-05 23:55:56 +0300

    add title to header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 
-const Header = () => {
+interface HeaderProps {
+  title?: string;
+}
+const Header = ({ title }: HeaderProps) => {
   return (
     <header className="">
-      <h1 className="">Todo App</h1>
+      <h1 className="">{title || "Todo App"}</h1>
       <nav
         className=""
         style={{
+          display: "flex",
+          gap: "1rem",
           borderBottom: "solid 1px",
           paddingBottom: "1rem",
         }}
